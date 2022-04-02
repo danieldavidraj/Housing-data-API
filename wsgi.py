10d161e620d6c757bff283e3a9dca68b5337e234
danieldavidraj <danieldavidraj23@gmail.com>
2022-04-02 21:23:34 +0530

    make it better

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from flaskr.main import app
+from flaskr.main import create_app
 
 import os
 import configparser
@@ -8,6 +8,7 @@ config = configparser.ConfigParser()
 config.read(os.path.abspath(os.path.join(".ini")))
 
 if __name__ == "__main__":
+    app = create_app()
     app.config['MONGO_URI'] = config['PROD']['DB_URI']
 
     app.run()
