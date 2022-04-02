8fb4d1418034ce68b4e7893902917dbaf2be9e30
danieldavidraj <danieldavidraj23@gmail.com>
2022-04-02 20:40:49 +0530

    make it better

--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,6 @@ config.read(os.path.abspath(os.path.join(".ini")))
 
 if __name__ == "__main__":
     app = create_app()
-    app.config['DEBUG'] = True
     app.config['MONGO_URI'] = config['PROD']['DB_URI']
 
     app.run()
